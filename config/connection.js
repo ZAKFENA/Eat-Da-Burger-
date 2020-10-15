298b45dfd723dac6df2614a3b907ab4d4803af67
Zekarias Negassa <zakfena@gmail.com>
2020-10-15 04:36:21 -0500

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,19 @@
-
 var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
   var connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
-  const connection = mysql.createConnection({
-    host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+};
+connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "userz",
+  password: "zuser12",
+  database: "burgers_db",
+});
 
-    // Your username
-    user: "userz",
-
-    // Your password
-    password: "zuser12",
-    database: "burgers_db",
-  });
-
-}
-
+var PORT = process.env.PORT || 3306;
 
 connection.connect(function (err) {
   if (err) {
