0d17c3d1c1dc7b166da22fcc9658c7d928a70bcd
Zekarias Negassa <zakfena@gmail.com>
2020-10-15 11:46:19 -0500

    re configured connection.js

--- config/connection.js
+++ config/connection.js
@@ -15,12 +15,6 @@ connection = mysql.createConnection({
 
 var PORT = process.env.PORT || 3306;
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
