20013a7777f7d5e94050df749683c0d0b9557b32
Zekarias Negassa <zakfena@gmail.com>
2020-10-16 12:14:46 -0500

    corrected connection code

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,18 @@ var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
   var connection = mysql.createConnection(process.env.JAWSDB_URL)
+  console.log("DB Connection Test")
+  console.log(connection)
 } else {
-
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "userz",
+    password: "zuser12",
+    database: "burgers_db",
+  });
 };
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "userz",
-  password: "zuser12",
-  database: "burgers_db",
-});
+
 
 var PORT = process.env.PORT || 3306;
 
